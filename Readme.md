0440e70ddb883ae31ca9bfb7258d180cc25e56c1
Aditya Vashishtha <vashishthaaditya20@gmail.com>
2018-05-16 15:48:09 +0530

    Config file added, minor code adjustments

--- Readme.md
+++ Readme.md
@@ -1,2 +1,19 @@
-## WallOnTime - Wallpaper with stats on linux
-This utility written in python change desktop wallpaper and show feeds on them.
+# WallOnTime: Unsplash based random wallpaper (Ubuntu)
+
+![Demo Usage](./images/demoUsageWallOnTime.png)
+
+WallOnTime is a python script that changes linux (tested on ubuntu 16.04) wallpaper randomly from unsplash based some specified theme.
+
+## Features
+  - Theme based wallpaper each time script run.
+  - New random quote from predefined sets of quote.
+  - Easy integration of RSS feeds.
+  - Configuration via json file.
+
+## Setup
+- Before running script change Font face location, size, box sizing, color, font color etc. from config as per need.
+- Choose theme in config file like (tech, computer, miami etc.).
+- Run `python main.py` (Proper Internet connection is needed).
+
+
+Credit to unsplash.com to provide such an awesome api for random stock photos.
--- main.py
+++ main.py
@@ -5,20 +5,38 @@ from PIL import ImageDraw
 from PIL import ImageFilter
 from PIL import ImageEnhance
 import os
+import json
 
 import quoteLoader
 import imageLoader
 import newsLoader
 
-# CONSTANT Declaration for usage -----START
-FONT_COLOR = (200,230,255)
-FONT_SIZE = 25
-FONT_SIZE_FEEDS = 20
-FONT_APPLE_GARAMOND = "/usr/share/fonts/truetype/raleway-elementary/Raleway-Light.ttf"
-BOX_SIZE = (1500,100,1900,400)
-NEWS_BOX_SIZE = (1500,450,1900,1030)
+#Loading configs from file
+CONFIG = {}
+with open('./config.json') as f:
+    CONFIG = json.load(f)
+THEME = CONFIG["image"]["theme"]
+RESOLUTION = CONFIG["image"]["resolution"]
+CARD_COLOR = (CONFIG["card"]["color"][0],CONFIG["card"]["color"][1],CONFIG["card"]["color"][2],CONFIG["card"]["color"][3])
+FONT_TYPE = CONFIG["font"]["fontLocation"]
+FONT_SIZE = CONFIG["font"]["fontSize"]
+FONT_COLOR = (CONFIG["card"]["fontColor"][0],CONFIG["card"]["fontColor"][1],CONFIG["card"]["fontColor"][2])
+FONT_SIZE_FEEDS = CONFIG["font"]["fontSizeFeeds"]
+BOX_SIZE = (CONFIG["card"]["quoteBoxSize"]["left"],
+            CONFIG["card"]["quoteBoxSize"]["top"],
+            CONFIG["card"]["quoteBoxSize"]["right"],
+            CONFIG["card"]["quoteBoxSize"]["bottom"])
+NEWS_BOX_SIZE = (CONFIG["card"]["feedBoxSize"]["left"],
+                    CONFIG["card"]["feedBoxSize"]["top"],
+                    CONFIG["card"]["feedBoxSize"]["right"],
+                    CONFIG["card"]["feedBoxSize"]["bottom"])
+MARGIN = 20
+QUOTE_START_CORDINATE = (CONFIG["card"]["quoteBoxSize"]["left"]+MARGIN,
+                    CONFIG["card"]["quoteBoxSize"]["top"]+MARGIN)
+IMAGE_URL = "https://source.unsplash.com/random/"+ RESOLUTION +"?"+ THEME
+FONT_APPLE_GARAMOND = FONT_TYPE 
 RESIZE_FACTOR_LOGO = 1
-IMAGE_URL = "https://source.unsplash.com/random/1920x1080"
+print IMAGE_URL
 IMAGE_PATH = "images/wallie.jpeg"
 # CONSTANT Declaration for usage -----END
 imageText='''
@@ -34,15 +52,16 @@ imageText,author = quoteLoader.loadQuote()
 # DONE # 1. Quote Loader                        #
 # DONE # 2. News Loader                         #
 # DONE # 3. Image Loader                        #
-# TODO # 4. Merger                              #
-# TODO # 5. Constant Storage                    #
+# DONE # 4. Merger                              #
+# TODO # 5. Constant and OOPS conversion        #
+# TODO # 6. Merging with the GUI and service    #
 # # # Structuring END ###########################
 
 imageLoader.loadImage(IMAGE_URL,IMAGE_PATH)
 
 img = Image.open("images/wallie.jpeg")
 img = img.convert("RGBA")
-font = ImageFont.truetype(FONT_APPLE_GARAMOND , FONT_SIZE)
+font = ImageFont.truetype(FONT_TYPE , FONT_SIZE)
 
 quote_logo_img = Image.open("images/quote-left.png")
 quote_logo_img = quote_logo_img.resize((int(quote_logo_img.size[0]*RESIZE_FACTOR_LOGO),int(quote_logo_img.size[1]*RESIZE_FACTOR_LOGO)))
@@ -58,13 +77,14 @@ im = im.filter(ImageFilter.BLUR)
 im = im.convert("RGBA")
 tmp = Image.new('RGBA', im.size, (0,0,0,0))
 draw = ImageDraw.Draw(tmp)
-draw.rectangle(((0, 0), im.size), fill=(50,50,50,127))
+draw.rectangle(((0, 0), im.size), fill=CARD_COLOR)
 im = Image.alpha_composite(im, tmp)
 draw = ImageDraw.Draw(im)
 draw.text((100,25), imageText , FONT_COLOR, font=font)
 draw.text((150,250),author,FONT_COLOR,font=font)
-img.paste(im,(1500,100,1900,400))
+img.paste(im,BOX_SIZE)
 
+'''Feeds Loading Stopped Currently
 news_im = img.crop(NEWS_BOX_SIZE)
 news_im = news_im.filter(ImageFilter.BLUR)
 news_im = news_im.filter(ImageFilter.BLUR)
@@ -93,17 +113,18 @@ for feed in feeds:
     for w in feed.split():
         word_count = word_count + 1
         char_count += len(w)
-        feed_line = feed_line+' '+w
-        if(word_count >= 4 or char_count >= 24):
+        if(word_count >= 4 or char_count >= 20):
             feed_line += "\n"
-            word_count = 1
-            char_count = 1                  
-    draw.text((158,30+(110*(count-1))), feed_line , FONT_COLOR, font=font)        
+            word_count = 0
+            char_count = 0  
+        feed_line = feed_line+' '+w                
+    draw.text((168,30+(110*(count-1))), feed_line , FONT_COLOR, font=font)        
     news_im.paste(temp_im,(30,30+(110*(count-1))))
     img.paste(news_im,NEWS_BOX_SIZE)
     count += 1
+Feeds Loading Stopped Currently'''
 
-img.paste(quote_logo_img,(1520,120),mask=quote_alpha)
+img.paste(quote_logo_img,QUOTE_START_CORDINATE,mask=quote_alpha)
 img.save("images/wallie_final.jpeg")
 
 os.system("/usr/bin/gsettings set org.gnome.desktop.background picture-uri file:$PWD/images/wallie_final.jpeg")
